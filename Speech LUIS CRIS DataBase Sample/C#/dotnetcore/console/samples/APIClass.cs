2312341af045c2d7c5960d4d1ef8e6ba580d3358
Ramamoorthy <ramamoorthy@jumpstartninja.com>
2018-10-16 19:52:02 +0530

    added C# code - Hybrid app for integrating CRIS and LUIS

---
+++ Speech LUIS CRIS DataBase Sample/C#/dotnetcore/console/samples/APIClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace samples
+{
+    public class APIClass
+    {
+        public string query { get; set; }
+
+        public string intent { get; set; }
+
+        public entities[] entities { get; set; }
+
+    }
+
+    public class entities
+    {
+        public string entity { get; set; }
+
+        public string type { get; set; }
+
+        public string startIndex { get; set; }
+
+        public string endIndex { get; set; }
+    }
+      
+
+}
---
+++ Speech LUIS CRIS DataBase Sample/C#/dotnetcore/console/samples/GetStatusClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace samples
+{
+    public class GetStatusClass
+    {
+        public string ResponseCode { get; set; }
+
+        public string Message { get; set; }
+
+        public data[] data { get; set; }
+
+    }
+
+    public class data
+    {
+        public string MemberID { get; set; }
+
+        public string MemberName { get; set; }
+    }
+}
